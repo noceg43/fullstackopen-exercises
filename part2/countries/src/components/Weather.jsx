dad21f9463fe5841516a6f0261cf52ba868280fa
noceg43 <noceg43@gmail.com>
2025-01-04 15:38:51 +0100

    add: ended 2.20

--- part2/countries/src/components/Weather.jsx
+++ part2/countries/src/components/Weather.jsx
@@ -1,33 +1,33 @@
 import weatherService from '../services/weather'
+import { useState, useEffect } from 'react'
 
 const Weather = ({ city }) => {
-    console.log('Weather', city);
-  
-      const [weather, setWeather] = useState([])
-    
-    
-      useEffect(() => {    
-          console.log('fetching weather...')
-          weatherService.getAll()
-            .then(response => {
-              setWeather(response)
-            })
-      }, [])
+  console.log('Weather', city);
+
+  const [weather, setWeather] = useState(null)
 
 
-    const temperature = weather.main.temp;
+  useEffect(() => {
+    console.log('fetching weather...')
+    weatherService.search(city)
+      .then(response => {
+        setWeather(response)
+      })
+  }, [])
 
-    const weatherImage = 'https://openweathermap.org/img/wn/' + weather.weather.icon + '@2x.png'
-    
-  
-    return (
-      <div>
-        <h3>Weather in {city}</h3>
-        <p>temperature {temperature} </p>
-        <img src={weatherImage} alt={`Weather icon of ${city}`} />
-      </div>
-    );
-  };
-  
-  export default Weather;
-  
+
+  const temperature = weather?.main.temp ?? null;
+  const u = `${f}`
+  const weatherImage = weather != null ? `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png` : null
+
+
+  return (
+    <div>
+      <h3>Weather in {city}</h3>
+      <p>temperature {temperature} </p>
+      <img src={weatherImage} alt={`Weather icon of ${city}`} />
+    </div>
+  );
+};
+
+export default Weather;
--- part2/countries/src/services/weather.js
+++ part2/countries/src/services/weather.js
@@ -1,18 +1,14 @@
 import axios from 'axios'
-const baseUrl = 'https://samples.openweathermap.org/data/2.5/weather'
+const baseUrl = 'https://api.openweathermap.org/data/2.5/weather'
 
 const api_key = import.meta.env.VITE_SOME_KEY
 
 
-const getAll = async () => {
-  const request = axios.get(baseUrl + 'all')
-  // return only the response.data short form
-  return request.then(response => response.data)
-}
 
 
-const search = async city => {
-  const request = axios.get(baseUrl + '?q=' + city + '&appid=' + api_key)
+const search = async (city) => {
+  const request = axios.get(baseUrl + '?q=' + city + '&appid=' + api_key + '&units=metric')
+
   return request.then(response => response.data)
 }
 
@@ -24,4 +20,4 @@ export default {
 }
 */
 // if the key name matches the content name use the short form
-export default { getAll, search }
+export default { search }
